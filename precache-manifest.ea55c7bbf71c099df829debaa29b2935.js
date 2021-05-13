0506b386c9af69a273e0d09904803c09c21d003a
limdp99 <47866457+limdp99@users.noreply.github.com>
2021-05-13 10:39:15 +0900

    Updates

---
+++ precache-manifest.ea55c7bbf71c099df829debaa29b2935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246b6fd62bd3a670214e311f1bd89e58",
+    "url": "/redeem_ef/index.html"
+  },
+  {
+    "revision": "dc726f600059af47261e",
+    "url": "/redeem_ef/static/css/main.c8326998.chunk.css"
+  },
+  {
+    "revision": "a74199fbe243160d88dd",
+    "url": "/redeem_ef/static/js/2.6c6a24be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redeem_ef/static/js/2.6c6a24be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc726f600059af47261e",
+    "url": "/redeem_ef/static/js/main.590aabca.chunk.js"
+  },
+  {
+    "revision": "e90d70aa0e73ad926183",
+    "url": "/redeem_ef/static/js/runtime-main.3df30c86.js"
+  }
+]);
